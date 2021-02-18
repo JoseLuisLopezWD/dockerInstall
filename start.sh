d834c56eb3b75eebba8e49bee24dc85c6ecc8a1b
José Luis López <jll@joseluis-lopez.com>
2021-02-18 22:19:47 +0100

    Fix

--- start.sh
+++ start.sh
@@ -8,6 +8,8 @@ apt-get update  && apt-get install -y \
     gnupg-agent \
     software-properties-common
 
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
